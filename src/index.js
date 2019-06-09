64f9ec0fbc9f8e3753a9e703f143845869eee9a5
zidol <ajh3166@naver.com>
2019-06-09 12:03:12 +0900

    redux store 설정

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {configureStore} from './module'
+import {Provider} from 'react-redux'
+const store = configureStore();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/**
+ * Provider에 store 연결을 해야
+ * 하위에 있는 컴포넌트에서 가져올 수 있음
+ */
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
---
+++ src/module/index.js
@@ -0,0 +1,22 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import penderMiddleware, {penderReducer} from 'redux-pender'
+
+
+export function configureStore(){
+    // 미들웨어 생성시 applyMiddleware() 안에 인자로 넣어줌
+    const middleware = applyMiddleware(penderMiddleware())
+
+    const composed = window.__REDUX_DEVTOOLS_EXTENSION__ ?
+        compose(
+            middleware,
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+        ) : 
+        middleware;
+    // createStore에 첫번째 인자는 리듀서, 두번째 인자는 미들웨어
+    return createStore(
+        combineReducers({
+            pender : penderReducer
+        }),
+        composed
+    )
+}
